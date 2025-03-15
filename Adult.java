af01708dc27be6f632e93184d58dec6c49d8d403
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-03-15 15:17:00 +0530

    problem by using if else conditions to get the person is adult or not

---
+++ Adult.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Adult {
+    public static void main(String args[]){ 
+        int age;
+        System.out.println("enter the age");
+        Scanner sc = new Scanner(System.in);
+        age = sc.nextInt();
+        if (age > 18) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("not adult");
+        }
+
+    }
+}
