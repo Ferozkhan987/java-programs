32f55b6ee04936acbc52d128353e943f43bd42f6
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-03-16 19:50:45 +0530

    pattern1 inverted trangle

---
+++ Pattern1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Pattern1 {
+    public static void main(String args[]) {
+        System.out.println("enter the number of stars");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = n; i > 0; i--) {
+            for (int j=i;j> 0 ; j--){ 
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
