517bf64a2ef7ca2ad0451ca748c32be2aa7c9a23
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-03-17 13:20:45 +0530

    added the new method by using or

--- Pattern5.java
+++ Pattern5.java
@@ -8,20 +8,14 @@ class Pattern5 {
         int m = sc.nextInt(); // || ||j == n || i == n
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (i == 1) {
+                if (i == 1 || j == 1 || i == n || j == m) {
                     System.out.print("* ");
-                } else if (i == n) {
-                    System.out.print("* ");
-                } else if (j == m) {
-                    System.out.print("* ");
-                } else if (j == 1) {
-                    System.out.print("* ");
-                } else {
+                }
+                else {
                     System.out.print("  ");
                 }
             }
             System.out.println(" ");
         }
 
-    }
-}
+}}
