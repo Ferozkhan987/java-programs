7a44a59cf77fd4b8458db911123fc7420b0089f2
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-03-20 21:07:38 +0530

    added the full diamond pattern

--- Pattern11.java
+++ Pattern11.java
@@ -14,6 +14,15 @@ class Pattern11 {
             }
             System.out.println();
         }
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
     }
 }
