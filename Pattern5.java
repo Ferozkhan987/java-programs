248cd2e6686b8af30d8c15b889319b84c59409cc
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-03-21 20:36:47 +0530

    added the new pattern

--- Pattern5.java
+++ Pattern5.java
@@ -10,12 +10,20 @@ class Pattern5 {
             for (int j = 1; j <= m; j++) {
                 if (i == 1 || j == 1 || i == n || j == m) {
                     System.out.print("* ");
-                }
-                else {
+                } else {
                     System.out.print("  ");
                 }
             }
             System.out.println(" ");
         }
+        System.out.println();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
 
-}}
+        }
+
+    }
+}
