dddf11ce88e90bd594ef0e1c40486cd99de0c9f2
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-03-21 20:29:53 +0530

    added the hallow daimond

--- Pattern11.java
+++ Pattern11.java
@@ -23,6 +23,32 @@ class Pattern11 {
             }
             System.out.println();
         }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                if (j == 0 || j == i - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                if (j == 0 || j == i - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
 
     }
 }
