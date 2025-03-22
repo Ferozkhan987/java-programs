6955f5b02973a7fef750b828b4f5ffed6725ef85
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-03-22 13:07:41 +0530

    added the factorial number

---
+++ Factorial.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Factorial {
+    public static void main(String args[]) {
+        System.out.println("enter the value of A ");
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = factorialnum(A);
+        System.out.println(B);
+
+    }
+
+    public static int factorialnum(int A) {
+        if (A == 0) {
+            return 1;
+        } else {
+            int fac = factorialnum(A - 1);
+            int B = A * fac;
+            return B;
+        }
+    }
+}
