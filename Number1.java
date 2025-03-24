b0142ee6d812774a075781b4ebf86e3425a3547c
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-03-24 20:41:40 +0530

    added the fibonace series

---
+++ Number1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Number1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        fabinocaniSeries(n);
+
+    }
+
+    public static void fabinocaniSeries(int n) {
+        int m = 0;
+        int s = 1;
+        int fac = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.print(" " + fac);
+            fac = m + s;
+            m = s;
+            s = fac;
+            
+        }
+    }
+}
