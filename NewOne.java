bc703649ace8ef1f51f7b62b355ec986a5f7671b
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-03-24 20:14:26 +0530

    added some more syntax

--- NewOne.java
+++ NewOne.java
@@ -5,12 +5,14 @@ class NewOne {
         System.out.println("enter the numbers 1 for the number 2 for exit");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int p = 0, ni = 0, z = 0;
+        int p = 0;
+        int ni = 0;
+        int z = 0;
         functionForCOunting(n);
-
+        System.out.println("the value of negitive num, positive num,zero are " + ni + " " + p + " " + z);
     }
 
-    public static void functionForCOunting(int n) {
+    public static int functionForCOunting(int n) {
         int p = 0, ni = 0, z = 0;
         Scanner sc = new Scanner(System.in);
         switch (n) {
@@ -19,22 +21,25 @@ class NewOne {
                 if (m > 0) {
                     p = p + 1;
                     functionForCOunting(m);
-                    
-                }  else if (m < 0) {
+                    return p;
+
+                } else if (m < 0) {
                     ni = ni + 1;
-                    int m = sc.nextInt();
-                } else (m == 0) {
+                    functionForCOunting(m);
+                    return ni;
+                } else if (m == 0) {
                     z = z + 1;
-                    int m = sc.nextInt();
+                    functionForCOunting(m);
+                    return z;
                 }
-                case 2 : {
-                    return;
-                }
-        
             }
-           
+            case 2: {
+                System.out.println("exiting");
+                return 0;
+            }
+
         }
-        
+
     }
 
 }
