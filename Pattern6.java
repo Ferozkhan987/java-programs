40246fdc195375f38d5c827f8452cb069762fb96
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-03-25 20:35:00 +0530

    added the new pattern for printing

--- Pattern6.java
+++ Pattern6.java
@@ -6,6 +6,9 @@ class Pattern6 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i = 1; i <= n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print("  ");
+            }
             for (int j = 1; j <= i; j++) {
 
                 System.out.print(j + " ");
