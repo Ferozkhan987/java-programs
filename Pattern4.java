73813026200fe4fb7e32c9a8f75ae216a024d66f
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-03-25 20:43:38 +0530

    added the hallow rectangle

--- Pattern4.java
+++ Pattern4.java
@@ -9,8 +9,12 @@ class Pattern4 {
         int m = sc.nextInt();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                System.out.print("*  ");
+                if (i == 0 || j == 0 || j == m - 1 || i == n - 1) {
+                    System.out.print("*  ");
 
+                } else {
+                    System.out.print("   ");
+                }
             }
             System.out.println();
         }
