ae79f9a0c864e14600d2e69aa411e25f03cc5ff5
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-03-25 20:28:22 +0530

    modified the pattern

--- Pattern7.java
+++ Pattern7.java
@@ -6,6 +6,9 @@ class Pattern7 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
             for (int j = 1; j <= n - i + 1; j++) {
 
                 System.out.print(j + " ");
