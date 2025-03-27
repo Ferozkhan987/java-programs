de4bf53cc9ce6e0c3d500b076fa14913d02014ab
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-03-27 20:33:50 +0530

    added the largest element checkoing function

---
+++ LargestElement.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class LargestElement {
+    public static int findLargest(int[] arr) {
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max)
+                max = num;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = { 3, 8, 2, 10, 5 };
+        System.out.println("Largest: " + findLargest(numbers));
+    }
+}
