25adad4cae74610f910d0001cb65b8d55ae8c0af
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-04-01 20:18:52 +0530

    added the prime num

--- PrimeCheck1.java
+++ PrimeCheck1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class PrimeCheck1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a number: is ");
         int num = scanner.nextInt();
         boolean isPrime = true;
 
