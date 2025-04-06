c0242ed788aadc68c048b963df0eab6779719e7e
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-04-06 21:34:26 +0530

    added the []

--- Arrays.java
+++ Arrays.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 class Arrays {
     public static void main(String args[]) {
-        int[] marks = new int[3];
+        int[] marks[] = new int[3];
 
         marks[1]=66;
         for(int i=0; i<3;i++){
         System.out.println(marks[1])
         }
     }
-    
+
 }
