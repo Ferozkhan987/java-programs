fbd1b03614bc3150dcf73356abe68f0518ac38bb
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-04-07 19:18:49 +0530

    2d array input

---
+++ Arrays2d.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+imoport java.util.*;
+public class Arrays2d {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        int[][] numbers = new int[row][col];
+        for(int i = 0;i<row;i++){
+            for(int j = 0;j<col;j++){
+                numbers[i][j]=sc.nextInt();
+            }
+        } 
+    }
+    
+}
