4e71218534ebbe85eb35969995f7b3a29a9bea3a
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-04-07 21:15:31 +0530

    added the string code basic

---
+++ Stringsss.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Stringsss {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String name = "Ferozkhan";
+        String fullname = "mantur";
+        System.out.println(name);
+    }
+    
+}
