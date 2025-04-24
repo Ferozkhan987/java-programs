c97e57726de841fdf91f857b914e684480c2d501
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-04-24 21:35:42 +0530

    getting the input from the user

--- neww.java
+++ neww.java
@@ -3,6 +3,9 @@ import java.util.*;
 public class neww {
     public static void main(STirngs args[]) {
         System.out.println("enter what do uh want");
+        Scanner sc = new Scanner(System.in);
+        int str = sc.nextLine();
+
     }
 
 }
