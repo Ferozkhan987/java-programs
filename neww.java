568dafddeda94f31b36cb21a3a9c84b9237dfaaf
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-04-24 21:38:28 +0530

    madee the code for cal

--- neww.java
+++ neww.java
@@ -5,7 +5,29 @@ public class neww {
         System.out.println("enter what do uh want");
         Scanner sc = new Scanner(System.in);
         int str = sc.nextLine();
-
+        System.out.println(str);
+        switch (str) {
+            case 'add':
+                result = num1 + num2;
+                break;
+            case 'sub':
+                result = num1 - num2;
+                break;
+            case 'mul':
+                result = num1 * num2;
+                break;
+            case 'div':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error:due TO Division by zero");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator");
+                return;
     }
 
+}
 }
