0efdd3353eeeca44f1c1869e53ea8d6f56bad046
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-04-25 20:45:24 +0530

    updated code

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 
 class SwapNumbers {
     public static void main(String args[]) {
-        int z, y, temp;
+        int x, a, z, y, temp;
         System.out.println("Enter z and y");
         Scanner sct = new Scanner(System.in); // User inputs two numbers
         z = sct.nextInt(); // User inputs two numbers
         y = sct.nextInt();
+        x = 12;
+        a = 21;
         System.out.println("Before Swapping\nz = " + z + "\ny = " + y);
+        System.out.println("Before Swapping\na = " + a + "\nx = " + x);
         temp = z; // Swapping is done
         z = y;
         y = temp;
