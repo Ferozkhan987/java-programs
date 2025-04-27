2d7070337603d928864cf40401dbfe1a60cd0d63
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-04-27 11:41:42 +0530

    added some libraries

--- GFG1.java
+++ GFG1.java
@@ -1,7 +1,8 @@
+import java.uitl.*;
+
 public class GFG1 {
 
-	public static void printSpiral(int size)
-	{
+	public static void printSpiral(int size) {
 
 		// Create row and col
 		// to traverse rows and columns
@@ -26,25 +27,25 @@ public class GFG1 {
 			// switch-case to determine the next index
 			switch (move) {
 
-			// If right, go right
-			case 'r':
-				col += 1;
-				break;
+				// If right, go right
+				case 'r':
+					col += 1;
+					break;
 
-			// if left, go left
-			case 'l':
-				col -= 1;
-				break;
+				// if left, go left
+				case 'l':
+					col -= 1;
+					break;
 
-			// if up, go up
-			case 'u':
-				row -= 1;
-				break;
+				// if up, go up
+				case 'u':
+					row -= 1;
+					break;
 
-			// if down, go down
-			case 'd':
-				row += 1;
-				break;
+				// if down, go down
+				case 'd':
+					row += 1;
+					break;
 			}
 
 			// Check if the matrix
@@ -59,8 +60,7 @@ public class GFG1 {
 				if (flag != 2) {
 
 					flag = 2;
-				}
-				else {
+				} else {
 
 					flag = 1;
 					sizeLeft -= 1;
@@ -69,25 +69,25 @@ public class GFG1 {
 				// switch-case to rotate the movement
 				switch (move) {
 
-				// if right, rotate to down
-				case 'r':
-					move = 'd';
-					break;
+					// if right, rotate to down
+					case 'r':
+						move = 'd';
+						break;
 
-				// if down, rotate to left
-				case 'd':
-					move = 'l';
-					break;
+					// if down, rotate to left
+					case 'd':
+						move = 'l';
+						break;
 
-				// if left, rotate to up
-				case 'l':
-					move = 'u';
-					break;
+					// if left, rotate to up
+					case 'l':
+						move = 'u';
+						break;
 
-				// if up, rotate to right
-				case 'u':
-					move = 'r';
-					break;
+					// if up, rotate to right
+					case 'u':
+						move = 'r';
+						break;
 				}
 			}
 		}
@@ -98,7 +98,7 @@ public class GFG1 {
 
 				int n = matrix[row][col];
 				System.out.print((n < 10) ? (n + " ")
-										: (n + " "));
+						: (n + " "));
 			}
 
 			System.out.println();
@@ -106,8 +106,7 @@ public class GFG1 {
 	}
 
 	// Driver Code
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 
 		// Get the size of size
 		int size = 5;
