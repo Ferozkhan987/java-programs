edede785153a881d2e1a91e9e607c13da4f19444
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-04-28 15:10:07 +0530

    added the printing the arraty

--- GFG2.java
+++ GFG2.java
@@ -1,10 +1,12 @@
 import java.util.*;
+import java.util.Arrays;
 
 class GFG2 {
     public static void main(String[] args) {
         Integer a[] = { 1, 2, 3, 4, 5 };
 
         // Sorting the array in descending order
+        System.out.println("printing the sorted array");
         Arrays.sort(a, Collections.reverseOrder());
 
         System.out.println(Arrays.toString(a));
