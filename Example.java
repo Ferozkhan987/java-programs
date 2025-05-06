50f20dbe13eb1e353dc9a84ee89d56e77a0ba5b2
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-05-06 20:37:24 +0530

    incremented the value of m

--- Example.java
+++ Example.java
@@ -4,7 +4,7 @@ class Example {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
         System.out.println("example file"+ m);
-        int s= m+1;
+        int s= m+2;
         System.out.println("example file"+ m);
 
     }}
