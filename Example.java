e555718de2bca2118c2223c782f14ef559ea009c
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-05-06 20:38:24 +0530

    user another input

--- Example.java
+++ Example.java
@@ -3,8 +3,10 @@ import java.util.*;
 class Example {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
+        int n = sc.nextInt();
         System.out.println("example file"+ m);
         int s= m+2;
+        
         System.out.println("example file"+ m);
 
     }}
