a4bc7b5790ad35e2f18d7cc97bd7822de88bceec
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-05-07 20:57:59 +0530

    commit

--- Example.java
+++ Example.java
@@ -4,7 +4,7 @@ class Example {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();// input from user
         int n = sc.nextInt();// 2 input from user
-        System.out.println("example file"+ m);
+        System.out.println("example file"+ m);// printing the 1 user input
         int s= m+2;
         int w = m+n;
         System.out.println("example file"+ m);
