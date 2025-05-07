824b0c6ef9f64f26df6912e582a81b4fc8340fa9
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-05-07 20:58:26 +0530

    increament

--- Example.java
+++ Example.java
@@ -5,7 +5,7 @@ class Example {
         int m = sc.nextInt();// input from user
         int n = sc.nextInt();// 2 input from user
         System.out.println("example file"+ m);// printing the 1 user input
-        int s= m+2;
+        int s= m+2;//increament the value
         int w = m+n;
         System.out.println("example file"+ m);
         System.out.println("example file"+ w);
