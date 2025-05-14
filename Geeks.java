1de3dace7d5f89368bd88ccecd1ba4f97efb7bfb
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-05-14 20:42:59 +0530

    using the console

--- Geeks.java
+++ Geeks.java
@@ -53,4 +53,14 @@ class Geeks {
         float b = s.nextFloat();
         System.out.println("You entered float " + b);
     }
+
+    public static void main(String[] args) {
+        // Using Scanner for Getting Input from User
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+        String s = scanner.nextLine();
+
+        System.out.println("You entered string " + s);
+    }
 }
