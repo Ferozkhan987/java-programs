3e12715835ab531b1dae1ce600df2cc68ef2146e
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-06-03 10:32:46 +0530

    corrected the operator

--- ZigZagPattern.java
+++ ZigZagPattern.java
@@ -2,7 +2,7 @@ public class ZigZagPattern {
     public static void main(String[] args) {
 
         int n = 10;
-        for (int i = 1; i <= 3; i++) {
+        for (int i = 1; i < 4; i++) {
             for (int j = 1; j <= n; j++) {
                 if ((i + j) % 4 == 0 || (i == 2 && j % 4 == 0)) {
                     System.out.print("* ");
