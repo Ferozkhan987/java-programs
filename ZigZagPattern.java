be4eccc745757ff84dd60557de4d8ce46d07f183
Ferozkhan987 <ferozkhanmantur@gmail.com>
2025-06-04 19:54:56 +0530

    operation correction

--- ZigZagPattern.java
+++ ZigZagPattern.java
@@ -4,7 +4,7 @@ public class ZigZagPattern {
         int n = 10;
         for (int i = 1; i < 4; i++) {
             for (int j = 1; j <= n; j++) {
-                if ((i + j) % 4 == 0) {
+                if ((i + j) % 4 == 0 || (i == 2 && j % 4 == 0)) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
